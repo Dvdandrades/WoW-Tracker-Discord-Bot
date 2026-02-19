cc15fa6d0022b4ba4e6eb911ab0c5acb3acc6db7
Dvdandrades <dvdandrades@gmail.com>
2026-02-19 14:26:59 +0100

    add linter and format

--- src/bot.py
+++ src/bot.py
@@ -6,6 +6,7 @@ from config import TOKEN, GUILD
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 
+
 class MyClient(discord.Client):
     async def on_ready(self):
         for guild in self.guilds:
@@ -13,12 +14,13 @@ class MyClient(discord.Client):
                 break
 
         print(
-            f'{self.user} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})\n'
+            f"{self.user} is connected to the following guild:\n"
+            f"{guild.name}(id: {guild.id})\n"
         )
 
         members = "\n - ".join([member.name for member in guild.members])
         print(f"Guild Members:\n - {members}")
+
 
 intents = discord.Intents.default()
 
